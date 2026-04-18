45ba4b2de4be958d4e7059bc6ff8ac2222d8ff96
AndreyMih159753 <andreyka_mih@mail.ru>
2026-04-18 18:08:52 +0300

    Set nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+
+  $development: {
+    devtools: { enabled: true }
+  },
+
+  $production: {
+    devtools: { enabled: false }
+  },
+
+  app: {
+    head: {
+      charset: 'UTF-8',
+      viewport: 'width=device-width, initial-scale=1.0',
+      title: 'True and the rainbow kingdom'
+    }
+  }
+});
